7088c49143346953222464c4f6b4a78dbe8f9d02
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2024-01-03 14:58:30 +0530

    Add files via upload

---
+++ class1SP.py
@@ -0,0 +1,21 @@
+#comments section
+'''ksjdfhjksdlfh
+kjshdflksdj
+lkjksdhfsd'''
+print("""hello world kshdkjfsd kjshdfksjdf kjssdfhjsd
+      jksdfhlisdgud
+      kjdbfihdhfbiudfd
+      jsdbfidsbfid""")
+
+x,y,z = 6,5,4
+print(x,y,z)
+sums = x + y + z
+div = x/y
+print("Sum is:",sums,sep = "=")
+print("Sum of", x ,",", y ,"and", z," is",sums)
+print("Div of %d and %d is %f"%(x,y,div))
+print("Div of {1} and {0} is {2}".format(y,x,div))
+#f-string
+print(f"Div of {x} and {y} is {div}")
+
+
