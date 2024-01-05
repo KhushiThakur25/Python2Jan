1a8092d83b7688ffe1843b951271e9918d7ea01f
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2024-01-05 14:05:19 +0530

    Add files via upload

---
+++ ForLoopSP.py
@@ -0,0 +1,26 @@
+'''for i in range(1,5,2):
+    print(i)'''
+#by default value of start is 0
+#by default value of step is 1
+
+
+'''j = 0
+while True:
+    print(j)
+    j += 1
+    if j > 10:
+        break
+print("loop exit")'''
+
+'''j = 0
+while j < 10:
+    print(j)
+    j += 1'''
+
+flag = True
+j = 0
+while flag:
+    print(j)
+    j += 1
+    if j > 5:
+        flag = False
